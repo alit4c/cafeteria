897063a60c04d4dbe2fe494a8fcdf74f024605f0
alit4c <ghass1053ds@gmail.com>
2025-06-08 01:58:49 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('cafeteria-cache-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icons/icon-192x192.png',
+        './icons/icon-512x512.png',
+        './screenshot1.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
